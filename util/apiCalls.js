97c5836ad62caea981243384ba864287e238211e
gerardonastri <gerardonastri06@gmail.com>
2024-07-02 15:56:16 +0200

    api url

--- util/apiCalls.js
+++ util/apiCalls.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const  BASE_URL = "https://modern-ecommerce-lovat.vercel.app/api/";
-const BASE_URL = "http://localhost:3000/api/";
+const BASE_URL = "https://modern-ecommerce-lovat.vercel.app/api/";
+// const BASE_URL = "http://localhost:3000/api/";
 
 export const axiosReq = axios.create({
   baseURL: BASE_URL,
